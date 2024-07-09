e3165ba24b5a70de9eb6f59b2e2e1b0473886e88
karthik3478 <148213933+karthik3478@users.noreply.github.com>
2024-07-09 08:55:59 +0000

    Add function to get internet context and other misc stuff

--- utils.py
+++ utils.py
@@ -4,27 +4,58 @@ from pathlib import Path
 import os
 import requests
 from googlesearch import search
+from bs4 import BeautifulSoup
+from langchain_community.vectorstores import FAISS
+from langchain.prompts import PromptTemplate
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import json
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
+
 
 
 
 load_dotenv(f"{Path.cwd()}/config.env")
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
+with open(f"{Path.cwd()}/prompts/search_term.txt",'r') as prompt:
+    search_prompt = prompt.read()
+
+with open(f"{Path.cwd()}/prompts/conditional_agent_prompt.txt",'r') as main_agent_prompt:
+    conditional_agent_prompt = main_agent_prompt.read()
+
+with open(f"{Path.cwd()}/prompts/code_prompt.txt",'r') as code_prompt:
+    claude_code_prompt = code_prompt
+
 model = genai.GenerativeModel('gemini-1.5-pro')
 model_flash = genai.GenerativeModel('gemini-1.5-flash')
+embedding_model = GoogleGenerativeAIEmbeddings(model="models/embedding-001",google_api_key=os.environ['GEMINI_API_KEY'])
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100,length_function=len)
+driver = uc.Chrome(headless=True)
 
-def internet_context(search_term):
+def internet_context(user_query): 
     context = {}
-    url_list = search(term=search_term, num_results=10)
+    parsed_urls = []
+    search_query_gen_prompt = PromptTemplate.from_template(template=search_prompt)
+    formatted_search_query_prompt = search_query_gen_prompt.format(question=user_query)
+    search_term = model_flash.generate_content(formatted_search_query_prompt,generation_config={"response_mime_type":"application/json"})
+    url_list = list(search(term=json.loads(search_term.text)["search_query"], num_results=4))
     for url in url_list:
-        print(url)
-        html_content = requests.get(url=url)
-        print(html_content.text)
+        driver.get(url)
+        parsed_url_text = driver.find_element(By.TAG_NAME,'body').text
+        text_splits = text_splitter.split_text(parsed_url_text)
+        db = FAISS.from_texts(text_splits, embedding=embedding_model)
+        test = db.similarity_search(user_query,k=2)
+        print(len(text_splits))
+        print(test)
+        #context[f"internet_context_{parsed_urls.index(url) + 1}"] = {"context" : ''.join(map(str,db.similarity_search(user_query,k=2))),"url" : url.metadata['source']}
+    #return context
+    
 
 def send_message(user_input,_):
-    context = internet_context(user_input["text"])
-    response = model.generate_content(user_input["text"])
-    return response.text
+    generated_message = model.generate_content(user_input["text"])
+    return generated_message.text
 
-
-internet_context("ai chatbots")
+test = internet_context("how to install windows on any pc ?")
+print(test)
